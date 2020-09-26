d33e37d40f19db823eb7acdb4c8f259ed84af17f
wic0144 <Arm_ruttanard@hotmail.com>
2020-09-26 20:41:55 +0700

    d

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/username/:usr', (req, res) => {
-    var usr_val = req.params.usr //ตัวแปรที่เก็บค่า usernames
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("post value /username/" + usr_val)
-    res.end()
-    })
-    .get('/username1/:usr', (req, res) => {
+  // .get('/username/:usr', (req, res) => {
+  //   var usr_val = req.params.usr //ตัวแปรที่เก็บค่า usernames
+  //   res.writeHead(200, { 'Content-Type': 'text/html' });
+  //   res.write("post value /username/" + usr_val)
+  //   res.end()
+  //   })
+    .get('/username/:usr', (req, res) => {
       var usr_val = req.params.usr //ตัวแปรที่เก็บค่า username
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write("get value /username/" + usr_val)
